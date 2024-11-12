c26037468d6af869ad3bc299912450ab25c5b457
AhmedHesham <ahmedhesham200987@gmail.com>
2024-11-12 22:38:26 +0200

    weather_model_almost_done <18>

---
+++ lib/models/weather_model.dart
@@ -0,0 +1,15 @@
+class WeatherModel {
+  final String date;
+  final int temp;
+  final double maxTemp;
+  final double minTemp;
+  final String WeatherstateNam;
+
+  WeatherModel({
+    required this.temp,
+    required this.date,
+    required this.maxTemp,
+    required this.minTemp,
+    required this.WeatherstateNam,
+  });
+}
--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,17 +1,33 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:weather_app_using_provider/models/weather_model.dart';
 
 class WeatherServices {
+  String BaseUrl = 'http://api.weatherapi.com/v1';
+  String APIKEY =
+      '19ad2dfcb89f404cbb9210423241111'; // Base URL for the weather API
   // Method to fetch weather data using the city name
-  void getWeatherData({required String cityName}) async {
+  Future<WeatherModel> getWeatherData({required String cityName}) async {
     // Constructing the URL for the GET request
-    Uri url = Uri.parse(
-        'http://api.weatherapi.com/v1/current.json?key=19ad2dfcb89f404cbb9210423241111&q=$cityName&aqi=no');
-    
+    Uri url =
+        Uri.parse('$BaseUrl/forecast.json?key=$APIKEY&q=$cityName&days=7');
+
     // Sending the GET request to the constructed URL
-    // The response is awaited since the call is asynchronous
     http.Response response = await http.get(url);
 
-    // The response object now holds the HTTP response data
-    // You can parse it further to extract specific weather information
+    // Parsing JSON response
+    Map<String, dynamic> data = jsonDecode(response.body);
+
+    // You can now access forecast data in the `data` map
+    var jsonData = data['forecast']['forecastday'][0];
+
+    WeatherModel weather = WeatherModel(
+        date: data['location']['localtime'],
+        temp: jsonData['avgtemp_c'],
+        maxTemp: jsonData['maxtemp_c'],
+        minTemp: jsonData['mintemp_c'],
+        WeatherstateNam: jsonData['condition']['text']);
+
+    return weather;
   }
 }
